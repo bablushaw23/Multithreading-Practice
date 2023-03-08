e778af3b52c7337ac06bee58a5f79f203b35dbe6
bablushaw23 <46132062+bablushaw23@users.noreply.github.com>
2023-03-08 21:00:34 +0530

    Add semaphore

---
+++ src/codes/semaphore/Connection.java
@@ -0,0 +1,47 @@
+package codes.semaphore;
+
+import java.util.concurrent.Semaphore;
+
+public class Connection {
+    private static Connection connection= new Connection();
+
+/*
+* Just as in OS, Semaphore is way to keep track of resource instances available to share.
+* Initialized with 10, is the initial number of instance available.
+* Once you take an instance, you call semaphore.acquire() and 10 will be 9 saying only 9 left
+* Once you are done with the instance and release it, alongside call semaphore.release() to make 9 back to 10 indicating the same.
+* When semaphore value reaches to 0 and semaphore.acquire() get called, then the thread goes in waiting until 
+* semaphore goes back to 1 or more indicating resources are available.
+*/    
+    private Semaphore semaphore= new Semaphore(10);
+
+    private Connection(){}
+
+    public static Connection getConnection(){
+        return connection;
+    }
+
+    private int currentConnections=0;
+
+    public void connect() {
+        try{
+            semaphore.acquire();
+            doConect();
+        }catch(InterruptedException ex){}
+        finally{
+            semaphore.release();
+        }
+    }
+
+    private void doConect() throws InterruptedException{
+        synchronized(this){
+            currentConnections++;       // depicting acquiring resource available. Note, I already decreased semaphore
+            System.out.println("Current connections active:"+currentConnections);
+        }
+        Thread.sleep(2000);     // utilising connection
+        
+        synchronized(this){
+            currentConnections--;       // releasing the connection
+        }
+    }
+}
---
+++ src/codes/semaphore/Main.java
@@ -0,0 +1,24 @@
+package codes.semaphore;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService exec= Executors.newCachedThreadPool();
+    
+        Runnable runnable= new Runnable(){
+            public void run(){
+                Connection.getConnection().connect();
+            }
+        };
+    
+        for(int i=0; i<200; i++){   // get 200 connections, u will experiance 10 connections then 2sec pause, agn 10 connections
+            exec.submit(runnable);
+        }
+        exec.shutdown();
+        exec.awaitTermination(1, TimeUnit.DAYS);   
+    }
+}
